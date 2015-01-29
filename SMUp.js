69f0990812936f64ea31bac9b622ce3324704e45
Eraw <mael.arnaud@gmail.com>
2015-01-29 22:02:07 +0100

    premiere ebauche

--- SMUp.js
+++ SMUp.js
@@ -1,7 +1,9 @@
 window.onload = function(){
 	var canvas = document.getElementById("canvas");
+	console.log(canvas);
 	
 	var init = function(){
+		console.log(canvas);
 		var mainship = new Ship(canvas, mainship_pattern);
 		mainship.build();
 	};
@@ -10,25 +12,26 @@ window.onload = function(){
 	
 	
 	var mainship_pattern = {
-		height : 12,
-		width : 7
+		width : 12,
+		height : 18
 	};
 	
 	function Ship (canvas, ship){
+		this.canvas = canvas;
 		this.width = ship.width;
 		this.height = ship.height;
 	};
 	
 	Ship.prototype.build = function(){
 		var ctx = this.canvas.getContext("2d");
-		var oX = 249; // Origin X
-		var oY = 10; // Origin Y
+		var oX = 10; // Origin X
+		var oY = canvas.height/2; // Origin Y
 		
 		ctx.beginPath();
 		ctx.moveTo(oX, oY); 
-		ctx.lineTo(oX, oY += mainship.width); // X: 249, Y: 17
-		ctx.moveTo(oX -= mainship.height/2, oY -= (mainship.width - (mainship.width/2))); // X: 247, Y: 15
-		ctx.lineTo(oX += mainship.height, oY);
+		ctx.lineTo(oX, oY += this.height); // X: 249, Y: 17
+		ctx.moveTo(oX -= this.width/1.5, oY -= this.height/2); // X: 247, Y: 15
+		ctx.lineTo(oX += this.width, oY);
 		ctx.stroke();
 	};
 	
