a831724a6bca447218aa502e1a9dd10bdd3362f5
Eraw <mael.arnaud@gmail.com>
2015-01-31 15:28:17 +0100

    Les vaisseaux ennemis avancent et popent correctement

--- SMUp.js
+++ SMUp.js
@@ -1,26 +1,28 @@
 window.onload = function(){
 	var canvas = document.getElementById("canvas");
-	var i = 0;var nextPlace = -1; // Prochaine case dans laquelle on placera la balle tirée, -1 signifiant un append
+	var nextPlace = -1; // Prochaine case dans laquelle on placera la balle tirée, -1 signifiant un append
+	var time_before_poping = 0;
+	var difficulty = 500;
 	
 	var init = function(){
 		var ctx = canvas.getContext("2d");
 		var mainship = new Ship(ctx, canvas, mainship_pattern);
 		mainship.build(mainship_pattern.width + 10, canvas.height/2);
-
+		var ennemies = [];
 		var direction = [0, 0];
 
-		// Nouvelle itération et affichage tous les 10 centièmes de sec
+		// Nouvelle itération et affichage tous les 10 millièmes de sec
 		timerId = setInterval(function(){
 			// Clear le canvas
 			ctx.clearRect(0, 0, canvas.width, canvas.height);
+			// Fait bouger le vaiseau dans la dernière direction pressée
 			mainship.move(direction[0], direction[1]);
-			i++;
-			if(i === 100){
-				mainship.shoot(nextPlace);
-				i = 0;
-			}
 			
+			shooting(mainship);
 			moveBullets(mainship);
+			popingEnnemies(ctx, ennemies);
+			movingEnnemies(ennemies);
+			resolvingShots(mainship, ennemies);
 			
 		}, 10);
 
@@ -42,12 +44,26 @@ window.onload = function(){
 	
 	
 	var mainship_pattern = {
-		width : 20, //12
-		height : 25, //18
+		width : 20,
+		height : 25,
 		speed : 0.5,
+		attack_speed : 100,
+		ennemy : false,
 		bullets : {
 			size : 1,
-			speed : 1,
+			speed : 1 // Nombre de cases parcourues par les balles à chaque tour
+		}
+	};
+	
+	var basic_ennemy_pattern = {
+		width : 20,
+		height : 25,
+		speed : 0.5,
+		attack_speed : 110,
+		ennemy : true,
+		bullets : {
+			size : 1,
+			speed : 2 // Nombre de cases parcourues par les balles à chaque tour
 		}
 	};
 	
@@ -57,6 +73,9 @@ window.onload = function(){
 		this.width = ship.width;
 		this.height = ship.height;
 		this.speed = ship.speed;
+		this.attack_speed = ship.attack_speed;
+		this.ennemy = ship.ennemy;
+		this.time_before_shooting = 0; // incrémenté toutes les centièmes de seconde, quand il atteint l'attack-speed, le vaisseau tire
 		this.bullets = ship.bullets;
 		this.oX = 0;
 		this.oY = 0;
@@ -67,20 +86,32 @@ window.onload = function(){
 	// Dessiner le vaisseau
 	Ship.prototype.build = function(x, y){
 		
-		if((this.oX > 0 && this.oX < this.canvas.width) || (this.oX == 0 && x > 0) || (this.oX == this.canvas.width && x < 0)){
+		if((this.oX > 0 && this.oX < this.canvas.width) || (this.oX == 0 && x > 0) || (this.oX == this.canvas.width && x < 0)){ // Si la queue et le cockpit du vaisseau ne touchent ni le bord droit ni le bord gauche, il avance
 			this.oX += x; // Origin X
 		};
-		if((this.oY > 0 && this.oY < this.canvas.height - this.height) || (this.oY == 0 && y > 0) || (this.oY == this.canvas.height - this.height && y < 0)){
+		if((this.oY > 0 && this.oY < this.canvas.height - this.height) || (this.oY == 0 && y > 0) || (this.oY == this.canvas.height - this.height && y < 0)){ // Idem sur l'axe Y
 			this.oY += y; // Origin X
 		};
+
+		var front;
+		var back;
+
+		// Construit les ennemies à l'envers
+		if(!this.ennemy){
+			back = this.width/1.5;
+			front = this.width/3;
+		} else {
+			back = this.width/3;
+			front = this.width/1.5;
+		}
 
 		this.ctx.beginPath();
 		// Barre verticale
 		this.ctx.moveTo(this.oX, this.oY);
 		// Barre horizontale
 		this.ctx.lineTo(this.oX, this.oY + this.height);
-		this.ctx.moveTo(this.oX - this.width/1.5, this.oY + this.height/2);
-		this.ctx.lineTo(this.oX + this.width/3, this.oY + this.height/2);
+		this.ctx.moveTo(this.oX - back, this.oY + this.height/2);
+		this.ctx.lineTo(this.oX + front, this.oY + this.height/2);
 		this.ctx.stroke();
 	};
 	
@@ -118,6 +149,14 @@ window.onload = function(){
 		this.x += this.speed;
 	};
 
+	// Permet au vaisseau de tirer
+	var shooting = function(ship){
+		ship.time_before_shooting++;
+		if(ship.time_before_shooting === ship.attack_speed){
+			ship.shoot(nextPlace);
+			ship.time_before_shooting = 0;
+		}
+	};
 
 	var moveBullets = function(ship){
 		for(var j = 0 ; j < ship.fired_bullets.length ; j++){
@@ -126,11 +165,40 @@ window.onload = function(){
 				ship.fired_bullets[j] = null;
 				nextPlace = j;
 			}
-			console.log("place " + nextPlace);
-			console.log("longueur " + ship.fired_bullets.length);
+			//console.log("place " + nextPlace);
+			//console.log("longueur " + ship.fired_bullets.length);
 			// Si la balle existe dans le tableau, la bouger
 			if(ship.fired_bullets[j] != null){
 				ship.fired_bullets[j].move();	
+			}
+		}
+	};
+
+	var popingEnnemies = function(ctx, e){
+		if(time_before_poping === difficulty){
+			var ennemy = new Ship(ctx, canvas, basic_ennemy_pattern);
+			e.push(ennemy);
+			ennemy.build(canvas.width, Math.floor((Math.random() * canvas.height-2)+1));
+			time_before_poping = 0;
+		} else {
+			time_before_poping++;
+		}
+	};
+	
+	var movingEnnemies = function(e){
+		for(var j = 0 ; j < e.length ; j++){
+			e[j].move(-(e[j].speed), 0);
+		}
+	};
+	
+	// Résoud les tirs alliés et ennemis
+	var resolvingShots = function(ms, e){
+		for(var i = 0 ; i < ms.fired_bullets.length ; i++){
+			for(var j = 0 ; j < e.length ; j++){
+				// Faire en sorte que le vaisseau et la balle dépopent lorsqu'ils se touchent
+				// Faire en sorte que les deux array ne se remplissent pas de manière illimitée
+				
+				//J'aurai probablement besoin de 2 fonctions : depopBullet et depopShip
 			}
 		}
 	};
